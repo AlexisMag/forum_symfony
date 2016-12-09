9b2e32889b5aea9c99e97f851e92da646fd4ff1d
Alexis MAGRON <alexis.magron@gmail.com>
2016-12-09 22:24:31 +0100

    forums page

--- src/ForumBundle/Controller/ForumController.php
+++ src/ForumBundle/Controller/ForumController.php
@@ -5,15 +5,48 @@ namespace ForumBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use ForumBundle\Entity\Forum;
 
 class ForumController extends Controller
 {
+    /**
+    * @Route ("/forum/test")
+    */
+    public function test(Request $request){
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT f.id, f.name
+            FROM ForumBundle:Forum f
+            WHERE f.slug = :slug1 OR f.slug = :slug2
+            ORDER BY f.slug DESC
+            '
+        )
+        ->setParameter('slug1', 'html-css')
+        ->setParameter('slug2', 'php');
+
+        $forums = $query->getResult();
+        dump($forums);
+
+
+        return new Response("ok");
+    }
+
     /**
     * All forums
     * @Route("/forum", name="list_forum")
     */
     public function forumAction(Request $request){
+        //Get all forums from database
+        $repository = $this->getDoctrine()->getRepository('ForumBundle:Forum');
 
+        $forums = $repository->findAll();
+
+        return $this->render("forum/forums.html.twig", array(
+            "forums" => $forums
+        ));
     }
 
     /**
@@ -21,7 +54,7 @@ class ForumController extends Controller
     * @Route("/forum/category/{slug}", name="subjects")
     */
     public function subjectsAction(Request $request){
-        
+
     }
 
     /**
---
+++ src/ForumBundle/Entity/Forum.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ForumBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Forum
+ *
+ * @ORM\Table(name="forum")
+ * @ORM\Entity(repositoryClass="ForumBundle\Repository\ForumRepository")
+ */
+class Forum
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=100, unique=true)
+     */
+    private $slug;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Forum
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Forum
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
